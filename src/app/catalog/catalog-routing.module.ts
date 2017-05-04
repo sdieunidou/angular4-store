202d0d3339243988ba2bf94adc66c84df2826ff8
Seb <trakiss@gmail.com>
2017-05-04 10:23:42 +0200

    add routings

--- src/app/catalog/catalog-routing.module.ts
+++ src/app/catalog/catalog-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import {RouterModule} from "@angular/router";
 import {CatalogRootComponent} from "./catalog-root/catalog-root.component";
+import {CatalogListComponent} from "./catalog-list/catalog-list.component";
+import {CatalogBookComponent} from "./catalog-book/catalog-book.component";
 
 const routes = [
-  {path: 'catalog', component: CatalogRootComponent }
+  {
+    path: 'catalog',
+    component: CatalogRootComponent,
+    children: [
+      {path: 'list', component: CatalogListComponent },
+      {path: 'book/:id', component: CatalogBookComponent }
+    ]
+  }
 ];
 
 @NgModule({
