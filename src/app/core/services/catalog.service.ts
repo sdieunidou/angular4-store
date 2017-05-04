48cc52a1a7f68cfe2e57c055289b3985693c2309
Seb <trakiss@gmail.com>
2017-05-04 10:48:23 +0200

    add getBook

--- src/app/core/services/catalog.service.ts
+++ src/app/core/services/catalog.service.ts
@@ -4,7 +4,8 @@ import "rxjs/add/operator/map";
 import {Observable} from "rxjs/Observable";
 import {Book} from "../model/book.model";
 
-const URL = 'https://api.mongolab.com/api/1/databases/sfbooks/collections/sfbooks/?apiKey=d3qvB8ldYFW2KSynHRediqLuBLP8JA8i';
+const URL = 'https://api.mongolab.com/api/1/databases/sfbooks/collections/sfbooks/';
+const API_KEY = '?apiKey=d3qvB8ldYFW2KSynHRediqLuBLP8JA8i';
 
 @Injectable()
 export class CatalogService {
@@ -13,7 +14,12 @@ export class CatalogService {
   ) { }
 
   getList(): Observable<Book[]> {
-    return this.http.get(URL)
+    return this.http.get(URL + API_KEY)
+      .map(response => response.json());
+  }
+
+  getBook(id: string): Observable<Book>  {
+    return this.http.get(URL + id + API_KEY)
       .map(response => response.json());
   }
 }
