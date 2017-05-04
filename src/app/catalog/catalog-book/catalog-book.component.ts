317bca8509c2baa30e4dfe516a47d0c1d8fe87ce
Seb <trakiss@gmail.com>
2017-05-04 11:06:02 +0200

    add book detail

--- src/app/catalog/catalog-book/catalog-book.component.ts
+++ src/app/catalog/catalog-book/catalog-book.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {CatalogService} from "../../core/services/catalog.service";
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs/Subscription";
+import {Observable} from "rxjs/Observable";
+import {Book} from "../../core/model/book.model";
 
 @Component({
   selector: 'app-catalog-book',
   templateUrl: './catalog-book.component.html',
   styleUrls: ['./catalog-book.component.css']
 })
-export class CatalogBookComponent implements OnInit {
+export class CatalogBookComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  public book: Observable<Book>;
 
-  constructor() { }
+  constructor(
+    private catalog: CatalogService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      params => this.book = this.catalog.getBook(params['id'])
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription && this.subscription.unsubscribe();
   }
 
 }
