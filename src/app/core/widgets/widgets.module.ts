c17ee6231b81be3a56388c889cd109a560a1bdaa
Seb <trakiss@gmail.com>
2017-05-04 14:47:53 +0200

    update

--- src/app/core/widgets/widgets.module.ts
+++ src/app/core/widgets/widgets.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TextRatingPipe } from './text-rating.pipe';
+import { HtmlRatingPipe } from './html-rating.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TextRatingPipe],
-  exports: [TextRatingPipe]
+  declarations: [TextRatingPipe, HtmlRatingPipe],
+  exports: [TextRatingPipe, HtmlRatingPipe]
 })
 export class WidgetsModule { }
