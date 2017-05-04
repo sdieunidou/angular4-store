608a81c58746a2516efaf669e71b1cd7df588441
Seb <trakiss@gmail.com>
2017-05-04 14:28:52 +0200

    update

--- src/app/core/services/catalog.service.ts
+++ src/app/core/services/catalog.service.ts
@@ -4,6 +4,7 @@ import {Observable} from "rxjs/Observable";
 import {Book} from "../model/book.model";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/filter";
+import "rxjs/add/operator/publishLast"
 
 const URL = 'https://api.mongolab.com/api/1/databases/sfbooks/collections/sfbooks/';
 const API_KEY = '?apiKey=d3qvB8ldYFW2KSynHRediqLuBLP8JA8i';
@@ -16,13 +17,27 @@ export interface BookPrevNext {
 
 @Injectable()
 export class CatalogService {
+  list$: Observable<Book[]>;
+
   constructor(
     private http: Http
-  ) { }
+  ) {
+    this.load();
+
+    setInterval(() => {
+        this.load();
+    }, 10000);
+  }
+
+  load() {
+    this.list$ = this.http.get(URL + API_KEY)
+      .map(response => response.json())
+      .publishLast()
+      .refCount();
+  }
 
   getList(): Observable<Book[]> {
-    return this.http.get(URL + API_KEY)
-      .map(response => response.json());
+    return this.list$;
   }
 
   getBook(id: string): Observable<BookPrevNext>  {
